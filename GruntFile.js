7530aa0d54fcc43e13bc5126579aad8945294a4d
sentsin <xianxin@layui.com>
2016-09-15 13:09:44 +0800

    从gitosc转义到github

---
+++ GruntFile.js
@@ -0,0 +1,44 @@
+﻿module.exports = function(grunt) {  
+
+    // Project configuration.  
+    grunt.initConfig({  
+        pkg: grunt.file.readJSON('package.json'),
+        
+        //压缩js - layer
+        uglify: {  
+            options: {
+                expand: true
+            },
+            'laypage.js': {
+                options: {
+                     banner: '/*! layPage-v<%= pkg.version %> <%= pkg.description %> License MIT  <%= pkg.homepage %> By <%= pkg.author %> */\n;'
+                },
+                src: './src/laypage.js',  
+                dest: './laypage.js'
+            }
+        },
+
+        //压缩css
+        cssmin: {
+            options : { 
+                compatibility : 'ie8', //设置兼容模式 
+                noAdvanced : true //取消高级特性 
+            }
+            ,layer: {
+                files: [{
+                    expand: true,
+                    cwd: './src/skin',
+                    src: ['*.css', '!*.min.css'],
+                    dest: './skin'
+                }]
+            }
+        }
+    });
+  
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    
+    grunt.registerTask('default', ['uglify', 'cssmin']);
+
+  
+};  
---
+++ README.md
@@ -0,0 +1,8 @@
+﻿
+## 简要
+layPage是一款多功能的js分页组件，即适用于异步分页，又可用于传统的整页刷新跳页，还支持信息流加载，并且可无缝迁移至Node.js平台。layPage不依赖于任何第三方库，直接拿来用即可，它的接口继承了layui系列组件的一贯简洁，极易上手。那么，从现在开始，将一切分页的任务交给layPage吧！
+
+
+[官网]：[http://laypage.layui.com](http://laypage.layui.com) 
+
+
